6ed911b1d9474ab976e6ace9ac7048199552066b
mohammad saeed taleghani <mohammadsaeedtaleghani1@gmail.com>
2024-05-23 00:29:00 +0330

    orderFinall

--- cafe game Fam Fun/order/urls.py
+++ cafe game Fam Fun/order/urls.py
@@ -1,15 +1,16 @@
 
 from django.urls import include, path
 from . import (views)
-# from rest_framework import routers
-# from .views import  ordersViewSet
+from rest_framework import routers
+from .views import  ordersListApiView , ordersViewSet
 
-# router = routers.DefaultRouter()
-# router.register(r'order', ordersViewSet)
+router = routers.DefaultRouter()
+router.register(r'order', ordersViewSet)
 
 
 urlpatterns = [
 
-    # path('orders/all/', views.ordersListApiView.as_view() ,name='get_all_orders'),
-    path('orders/addnew/', views.ordersViewSet.as_view('post')),
+    path('orders/all/', ordersListApiView.as_view() ,name='get_all_orders'),
+    path('orders/addnew/', ordersViewSet.as_view('post'),name='post'),
+    path('orders/' , include(router.urls))
 ]
--- cafe game Fam Fun/order/views.py
+++ cafe game Fam Fun/order/views.py
@@ -4,29 +4,34 @@ from rest_framework import status
 from .models import orders
 from .serializers import ordersSerializer
 from rest_framework import viewsets
+from django.http import HttpResponse
 
 class ordersViewSet(viewsets.ModelViewSet):
    
     
    
-   queryset = orders.objects.all()
-   serializer_class = ordersSerializer
+    queryset = orders.objects.all()
+    serializer_class = ordersSerializer
+    http_method_names = ['post']
 
-   def create(self, request, *args, **kwargs):
+    def post(self, request):
+        return HttpResponse('POST request')
+
+    def create(self, request, *args, **kwargs):
         
         queryset = orders.objects.all()
         serializer = ordersSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# class ordersListApiView(APIView):
-#     # add permission to check if user is authenticated
+class ordersListApiView(APIView):
+    # add permission to check if user is authenticated
      
 
-#     # 1. List all
-#     def post(self, request, *args, **kwargs):
+    # 1. List all
+    def get(self, request, *args, **kwargs):
         
-#         queryset = orders.objects.all()
-#         serializer = ordersSerializer(queryset, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+        queryset = orders.objects.all()
+        serializer = ordersSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
