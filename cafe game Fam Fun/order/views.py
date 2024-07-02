cad6225ff614e0e13417d8b25454d1d182c7a852
mohammad saeed taleghani <mohammadsaeedtaleghani1@gmail.com>
2024-07-02 21:32:26 +0330

    555

--- cafe game Fam Fun/order/views.py
+++ cafe game Fam Fun/order/views.py
@@ -74,19 +74,19 @@ class getPrices(APIView):
         date_flag = None
 
         for order in orders_list:
-            created_date = order['created'].date()
+            created_date = orders['created'].date()
             print(f"Processing order: {order}")  # چاپ داده‌های پردازش شده
             if start_date <= created_date <= end_date:
                 if date_flag is None or created_date == date_flag:
-                    sum_price += order['price']
+                    sum_price += orders['price']
                     date_flag = created_date
                 else:
                     response_data.append({
-                        'name': order['name'],
+                        'name': orders['name'],
                         'date': date_flag,
                         'price': sum_price
                     })
-                    sum_price = order['price']
+                    sum_price = orders['price']
                     date_flag = created_date
 
         # اضافه کردن آخرین روز
