f599b1a64eea3f9915739e953396bfdd637762c1
mohammad saeed taleghani <mohammadsaeedtaleghani1@gmail.com>
2024-07-03 21:16:27 +0330

    deletingJalali

--- cafe game Fam Fun/order/views.py
+++ cafe game Fam Fun/order/views.py
@@ -14,7 +14,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import orders
 from datetime import datetime
-from jalali_date import date2jalali
 
 class ordersViewSet(viewsets.ModelViewSet):
    
@@ -93,7 +92,7 @@ class getPrices(APIView):
                     print(f"we are in else and the order name is : {order['name']} and date_flag is : {date_flag} and sum_price is {sum_price}")
                     response_data.append({
                         'name': order['name'],
-                        'date': date2jalali(date_flag),
+                        'date': date_flag,
                         'price': sum_price
                     })
                     sum_price = int(order['price'])
@@ -104,7 +103,7 @@ class getPrices(APIView):
         if date_flag is not None:
             response_data.append({
                 'name': orders_list[c]['name'],
-                'date': date2jalali(date_flag),
+                'date': date_flag,
                 'price': sum_price
             })
 
@@ -155,7 +154,7 @@ class getPricesAll(APIView):
                 else:
                     print(f"date_flag is : {date_flag} and sum_price is {sum_price}")
                     response_data.append({
-                        'date': date2jalali(date_flag),
+                        'date': date_flag,
                         'price': sum_price
                     })
                     sum_price = int(order['price'])
@@ -165,7 +164,7 @@ class getPricesAll(APIView):
         # اضافه کردن آخرین روز
         if date_flag is not None:
             response_data.append({
-                'date': date2jalali(date_flag),
+                'date': date_flag,
                 'price': sum_price
             })
 
